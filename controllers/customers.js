f5c379d2666524b3b16346d2fec1a29fc45a92c3
smrk <rodriguezsm72@gmail.com>
2020-11-23 09:35:03 -0300

    GetAllCustomers

---
+++ controllers/customers.js
@@ -0,0 +1,25 @@
+const express = require('express');
+
+const router = express.Router();
+const customers = require('../data/customers.json');
+
+const idFilter = req => customer => customer.id === parseInt(req.params.id);
+
+//Get All Customers
+router.get('/', (req, res) => {
+    res.json(customers)
+});
+
+//Get Customer By ID
+router.get('/:id', (req, res) => {
+    const found = customers.some(idFilter(req));
+
+    if (found) {
+        res.json(customers.filter(idFilter(req)));
+    } else {
+        res.status(400).json({ msg: `No customer with the id of ${req.params.id}` });
+    }
+});
+
+
+module.exports = router;
