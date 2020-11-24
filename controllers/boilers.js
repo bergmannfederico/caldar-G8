15dd8a50e9c1c9cabfa2f2b38ee86b12ef1b7d15
Nahuel <nahuelmartindacri@gmail.com>
2020-11-24 18:02:26 -0300

    PS8-004: add get boiler by skillsId and get boiler by description

--- controllers/boilers.js
+++ controllers/boilers.js
@@ -12,13 +12,39 @@ router.get('/', (require, response) => {
 // Get boiler by ID
 
 router.get('/id/:id', (require, response) => {
-    const idFilter = (require) => (boiler) => boiler.id === (require.params.id);
+    const idFilter = (require) => (boiler) => boiler.id === parseInt(require.params.id);
     const element = boilers.some(idFilter(require));
 
     if(element == true) {
         response.json(boilers.filter(idFilter(require)));
     } else {
-        response.status(400).json({ msg: `No boiler found with the id ${req.params.id}`});
+        response.status(400).json({ msg: `No boiler found with the id ${require.params.id}`});
+    }
+})
+
+// Get boiler by attribute: skillsId
+
+router.get('/skillsId/:skillsId', (require, response) => {
+    const idFilter = (require) => (boiler) => boiler.skillsId === (require.params.skillsId);
+    const element = boilers.some(idFilter(require));
+
+    if (element == true) {
+        response.json(boilers.filter(idFilter(require)));
+    } else {
+        response.status(400).json({ msg: `No boiler found with the skillsId ${require.params.skillsId}`})
+    }
+})
+
+// Get boiler by attribute: description
+
+router.get('/description/:description', (require, response) => {
+    const idFilter = (require) => (boiler) => boiler.description === (require.params.description);
+    const element = boilers.some(idFilter(require));
+
+    if (element == true) {
+        response.json(boilers.filter(idFilter(require)));
+    } else {
+        response.status(400).json({ msg: `No boiler found with the description ${require.params.description}`})
     }
 })
 
