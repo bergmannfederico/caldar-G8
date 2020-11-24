3a4f5447ff56804b6b0dd3cc82750bb6dc8f3d60
agustinbaffo <agusbaffo@hotmail.com>
2020-11-24 12:54:08 -0300

    Get appointments by attributes

--- controllers/appointments.js
+++ controllers/appointments.js
@@ -2,16 +2,45 @@ const express = require('express');
 const router = express.Router();
 const fs = require('fs');
 let appointmentsData = JSON.parse(fs.readFileSync('./data/appointments.json'));
-/*
-// Get all appointments
+
+// getAllAppointments
 router.get('/', (req, res) =>{
     res.send(appointmentsData);
 });
 
-// getAllAppointments
-router.get('/getAllAppointments', (req, res) =>{
-    res.send(appointmentsData);
-});*/
+
+// getAppointmentByAttribute
+router.get('/attributes', (req, res) => {   
+    let filterData = appointmentsData; 
+    let flag_filter = false;
+    if(req.query.id){
+        filterData = filterData.filter(appointment => appointment.id === parseInt(req.query.id));
+        flag_filter = true;
+    }
+    if(req.query.buildingId){
+        filterData = filterData.filter(appointment => appointment.buildingId === parseInt(req.query.buildingId));
+        flag_filter = true;
+    }
+    if(req.query.boilerId){
+        filterData = filterData.filter(appointment => appointment.boilerId === parseInt(req.query.boilerId));
+        flag_filter = true;
+    }
+    if(req.query.start_timestamp){
+        filterData = filterData.filter(appointment => appointment.start_timestamp === String(req.query.start_timestamp));
+        flag_filter = true;
+    }
+    if(req.query.end_timestamp){
+        filterData = filterData.filter(appointment => appointment.end_timestamp === String(req.query.end_timestamp));
+        flag_filter = true;
+    }
+
+    if(flag_filter){
+        res.json(filterData);
+    }else{
+        res.status(400).json({msg: `Attribute not found`}); 
+    }    
+});
+    
 
 // getAppointmentById
 router.get('/:id', (req, res) =>{
