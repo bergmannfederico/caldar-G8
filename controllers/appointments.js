bc958c3e088d07b149abc692863a7e8845082e9f
agustinbaffo <agusbaffo@hotmail.com>
2020-11-24 11:43:53 -0300

    Get appointments by ID

--- controllers/appointments.js
+++ controllers/appointments.js
@@ -2,10 +2,26 @@ const express = require('express');
 const router = express.Router();
 const fs = require('fs');
 let appointmentsData = JSON.parse(fs.readFileSync('./data/appointments.json'));
-
+/*
 // Get all appointments
 router.get('/', (req, res) =>{
     res.send(appointmentsData);
+});
+
+// getAllAppointments
+router.get('/getAllAppointments', (req, res) =>{
+    res.send(appointmentsData);
+});*/
+
+// getAppointmentById
+router.get('/:id', (req, res) =>{
+    const found = appointmentsData.some(appointment => appointment.id === parseInt(req.params.id));
+
+    if (found){
+        res.json(appointmentsData.filter(appointment => appointment.id === parseInt(req.params.id))); 
+    } else{
+        res.status(400).json({msg: `Appointments with ID = ${req.params.id} not found`});
+    }
 });
 
 module.exports = router;
