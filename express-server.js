91ea6c5f42933e8568ca186a88f47de44698d700
Matias Dominguez Alonso <matiasdominguez2@gmail.com>
2020-11-24 19:37:48 -0300

    add-server-express

--- express-server.js
+++ express-server.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const cors = require('cors')
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-})
 
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+
+// Settings
+app.set('port', process.env.PORT || 3000)
+app.set('json spaces', 2)
+app.use(cors())
+
+//Middlewares
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+//Routes
+app.use('/api/technicians',require('../caldar-G8/controllers/techniciansRoutes'));
+
+// Starting server
+app.listen(app.get('port'), () =>{
+    console.log('Server on port 3000')
 })
