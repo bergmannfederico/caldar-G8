d9657d82710ae9a792fb5071a65b46d8b429da85
Nahuel <nahuelmartindacri@gmail.com>
2020-11-28 10:38:10 -0300

    PS8-009: modified index-data file

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('hi');
+const express = require('express');
+
+const app = express();
+
+app.use('/boilers-data', require('./controllers/boilers-data'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
