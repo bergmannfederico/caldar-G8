6347683d5e0a07e68d03ff99f4e889c709eba86b
Nahuel <nahuelmartindacri@gmail.com>
2020-11-28 19:04:45 -0300

    modified routes

--- index.js
+++ index.js
@@ -1,17 +1,12 @@
-<<<<<<< HEAD
-const express = require('express');
+//<<<<<<< HEAD
+/*const express = require('express');
 
 const app = express();
 
 app.use('/boilers-data', require('./controllers/boilers-data'));
 
-=======
-const express = require("express");
 const path = require("path");
-const customers = require("./data/customers.json");
-
-const app = express();
-
+const customers = require("./data/boilers-data.json");
 
 //BodyParser
 app.use(express.json());
@@ -19,7 +14,16 @@ app.use(express.urlencoded({ extended: false }));
 
 //Customers API Routes
 app.use('/customers', require('./controllers/customers.js'));
->>>>>>> 82677c21728738ab215ec912d9c52694b1a26bae
+//>>>>>>> 82677c21728738ab215ec912d9c52694b1a26bae
 const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));*/
+const express = require('express');
+
+const app = express();
+
+app.use('/boilers-data', require('./controllers/boilers-data'));
+
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
