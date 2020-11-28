f9c8bc4642ea7aec560d529089eea1ed1bd2aad1
Nahuel <nahuelmartindacri@gmail.com>
2020-11-28 19:57:51 -0300

    modified files

--- express-server.js
+++ express-server.js
@@ -1,21 +1,27 @@
 const express = require('express');
 const app = express();
-//const cors = require('cors')
+
+const cors = require('cors');
+
 const port = 3000;
 
 
 
 // Settings
-//app.set('port', process.env.PORT || 3000)
-//app.set('json spaces', 2)
-//app.use(cors())
+
+
+app.set('port', process.env.PORT || 3000);
+app.set('json spaces', 2);
+app.use(cors());
+
 
 //Middlewares
 //app.use(express.urlencoded({extended: false}));
 //app.use(express.json());
 
 // Technicians API Routes
-app.use('/technicians', require('./controllers/techniciansRoutes'));
+app.use('/technicians',require('./controllers/technicians'));
+
 
 // Building API Routes
 app.use('/buildings', require('./controllers/buildings'));
@@ -23,12 +29,20 @@ app.use('/buildings', require('./controllers/buildings'));
 //Customers API Routes
 app.use('/customers', require('./controllers/customers'));
 
-//app.listen(port, () => {
-//    console.log(`Example app listening at http://localhost:${port}`);
-//})
+
 //Boilers API routes
 app.use('/boilers-data', require('./controllers/boilers-data'));
 
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+// Appointments API Routes
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+app.use('/appointments', require('./controllers/appointments'));
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+})
+
