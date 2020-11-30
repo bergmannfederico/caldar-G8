c9e4cd667006b58bfd3c237c4301359ecc38fb33
Matias Dominguez Alonso <matiasdominguez2@gmail.com>
2020-11-30 10:45:17 -0300

    index.js

--- app/models/index.js
+++ app/models/index.js
@@ -3,7 +3,7 @@ const db = {};
 db.mongoose = mongoose;
 db.url = 'mongodb+srv://admin:admin@cluster0.mekob.mongodb.net/CaldAR?retryWrites=true&w=majority';
 db.building = require('./buildings.js')(mongoose);
-db.building = require('./technicians')(mongoose);
+db.technicians = require('./technicians')(mongoose);
 
 
 
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const port = 5000;
 const router = require('./app/routes');
 const bodyParser = require('body-parser');
+const db = require('./app/models')
 
 // Settings
 app.use(bodyParser.json());
@@ -11,6 +12,31 @@ app.use(express.urlencoded({extended:false}));
 app.set('port', process.env.PORT || 3000);
 app.set('json spaces', 2);
 app.use(cors());
+app.use(router);
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+
+    db.mongoose
+        .connect(db.url, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
+        .then(() => {
+            console.log('Connected to the database!')
+        })
+        .catch((err) => {
+            console.log('Cannot connect to the database', err);
+            process.exit();
+        })
+
+
+// Static files
+app.use(express.static('public'));
+
 app.use(router);
 
 app.listen(port, () => {
