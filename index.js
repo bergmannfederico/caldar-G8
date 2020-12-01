f30d875386429204377806fd04ad7aa138668d75
FedericoB <federicob@gpl.com.ar>
2020-12-01 16:01:00 -0300

    Buildings methods and MongoDB connection

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const port = 5000;
 const router = require('./app/routes');
 const bodyParser = require('body-parser');
+const db = require('./app/models');
 
 // Settings
 app.use(bodyParser.json());
@@ -13,6 +14,19 @@ app.set('json spaces', 2);
 app.use(cors());
 app.use(router);
 
+db.mongoose
+    .connect(db.url, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => {
+        console.log('Connected to database!');
+    })
+    .catch(err => {
+        console.log('Cannot connected to database ', err);
+        process.exit();
+    });
+
 app.listen(port, () => {
     console.log(`Server started on port: ${port}`);
-})
+});
