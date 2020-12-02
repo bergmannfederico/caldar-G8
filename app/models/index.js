e8903d90cb6ae23287c3c880d4610e4e4722affa
Matias Dominguez Alonso <matiasdominguez2@gmail.com>
2020-12-02 17:49:46 -0300

    fix-//

--- app/models/index.js
+++ app/models/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const db = {};
 db.mongoose = mongoose;
 db.url = 'mongodb+srv://admin:admin@cluster0.mekob.mongodb.net/CaldAR?retryWrites=true&w=majority';
-//db.building = require('./buildings.js')(mongoose);
+db.building = require('./buildings.js')(mongoose);
 db.technicians = require('./technicians')(mongoose);
 
 
