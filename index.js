4fe74005c09c7399d51c64fc8ce383f653af0445
Matias Dominguez Alonso <matiasdominguez2@gmail.com>
2020-12-02 18:32:19 -0300

    fix-PR-sintaxis

--- index.js
+++ index.js
@@ -4,10 +4,9 @@ const cors = require('cors');
 const port = 5000;
 const router = require('./app/routes');
 const bodyParser = require('body-parser');
-const db = require('./app/models')
+const db = require('./app/models');
 
 // Settings
-app.use(bodyParser.json());
 app.use(express.urlencoded({extended:false}));
 app.set('port', process.env.PORT || 3000);
 app.set('json spaces', 2);
@@ -20,18 +19,18 @@ app.use(bodyParser.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended: true}));
 
-    db.mongoose
-        .connect(db.url, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        })
-        .then(() => {
-            console.log('Connected to the database!')
-        })
-        .catch((err) => {
-            console.log('Cannot connect to the database', err);
-            process.exit();
-        })
+db.mongoose
+    .connect(db.url, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => {
+          console.log('Connected to the database!')
+    })
+    .catch((err) => {
+        console.log('Cannot connect to the database', err);
+           process.exit();
+    })
 
 
 // Static files
