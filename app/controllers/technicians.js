eedb55ffc512966de46cec874702fd933c74c2ad
Matias Dominguez Alonso <matiasdominguez2@gmail.com>
2020-12-02 09:12:14 -0300

    fix-sintaxis

--- app/controllers/technicians.js
+++ app/controllers/technicians.js
@@ -1,5 +1,5 @@
 require('slf4n-logging');
-const logger = LoggerFactory.getLogger('Technicians')
+const logger = LoggerFactory.getLogger('Technicians');
 const db = require('../models');
 const Technicians = db.technicians;
 
@@ -16,12 +16,11 @@ exports.findAll = (req, res) =>{
             })
         })
 };
-
 //Get technicians by Attribute
 exports.findByAttr = (req, res) => {
     if(!(req.query.first_name || req.query.last_name || req.query.email || req.query.typeIds || req.query.skillsId || req.query.hour_rate || req.query.daily_capacity)){
-        logger.error(`The attr send on the URL is not compatible with technicians`);
-        return res.status(400).json({msg: `Attribute incompatible with technicians`})
+        logger.error(`The attribute send on the URL is not compatible with technicians`);
+        return res.status(400).json({msg: `The attribute is incompatible with technicians`})
     }
     if(req.query.typeIds){
         req.query.typeIds = parseInt(req.query.typeIds);
@@ -45,7 +44,6 @@ exports.findByAttr = (req, res) => {
             });
         });
 };
-
 //Get technicians by ID
 exports.findOne = (req, res) =>{
     Technicians.findOne({id: req.params.id})
@@ -63,15 +61,13 @@ exports.findOne = (req, res) =>{
             })
         })
 };
-
 //Create a new technician
 exports.create = (req, res) => {
     logger.info('Endpoint called: createTechnician');
     if(!req.body.first_name || !req.body.last_name || !req.body.email || !req.body.typeIds || !req.body.skillsId || !req.body.hour_rate || !req.body.daily_capacity || !req.body.id){
         return res.status(400).send({message: 'Specify all attributes to create a new technician'});
     }
-
-    // Create technician object
+    // Create new technician object
     const technician = new Technicians({
         id: req.body.id,
         first_name: req.body.first_name,
@@ -82,7 +78,6 @@ exports.create = (req, res) => {
         hour_rate: req.body.hour_rate,
         daily_capacity: req.body.daily_capacity
     });
-
     technician.save(technician)
         .then(data => {
             logger.info(`Creating technician with ID  ${req.body.id}`);
@@ -95,31 +90,13 @@ exports.create = (req, res) => {
             });
         });
 };
-
-//Delete technicians
-exports.delete = (req, res) => {
-    logger.info('Endpoint called: deleteTechnicianById');
-    const id = req.params.id;
-    Technicians.findOneAndDelete({id}, {useFindAndModify: false})
-        .then(() => {
-            logger.info(`Deleting s with ID equal to ${req.params.id}`);
-            res.send({message: 'Technician removed'})
-        })
-        .catch(() => {
-            logger.error(`Error trying to delete technician with ID=` + id);
-            return res.status(500).json({message: 'Error trying to delete technician with ID=' + id});
-        });
-};
-
 //Update technician by ID
 exports.update = (req, res) => {
     logger.info('Endpoint called: updateTechnician')
     if(!req.body.first_name || !req.body.last_name || !req.body.email || !req.body.typeIds || !req.body.skillsId || !req.body.hour_rate || !req.body.daily_capacity || !req.body.id){
         return res.status(400).send({message: 'Specify all attributes to update a technician'});
     }
-
     const id = req.params.id;
-
     Technicians.findOneAndUpdate({id}, req.body, { useFindAndModify: true })
         .then(data => {
             if(!data){
@@ -137,6 +114,19 @@ exports.update = (req, res) => {
             });
         });
 };
-
+//Delete technicians
+exports.delete = (req, res) => {
+    logger.info('Endpoint called: deleteTechnicianById');
+    const id = req.params.id;
+    Technicians.findOneAndDelete({id}, {useFindAndModify: false})
+        .then(() => {
+            logger.info(`Deleting s with ID equal to ${req.params.id}`);
+            res.send({message: 'Technician removed'})
+        })
+        .catch(() => {
+            logger.error(`Error trying to delete technician with ID=` + id);
+            return res.status(500).json({message: 'Error trying to delete technician with ID=' + id});
+        });
+};
 
 
--- app/routes/technicians.js
+++ app/routes/technicians.js
@@ -3,19 +3,14 @@ const technicians = require('../controllers/technicians.js');
 
 // Get all technicians
 router.get('/', technicians.findAll);
-
 //Get technicians by Attribute
 router.get('/attributes', technicians.findByAttr);
-
-//Create a new technicians
-router.post('/', technicians.create);
-
 //Get technicians by ID
 router.get('/:id', technicians.findOne);
-
+//Create a new technicians
+router.post('/', technicians.create);
 //Update technicians by ID
 router.put('/:id', technicians.update);
-
 //Delete technicians
 router.delete('/:id', technicians.delete);
 
