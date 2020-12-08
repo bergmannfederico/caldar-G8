8687f949875f2afa619b453b8ed4b2d3d368f70c
Matias Dominguez Alonso <matiasdominguez2@gmail.com>
2020-12-08 19:04:38 -0300

    add-heroku-files

--- app/routes/technicians.js
+++ app/routes/technicians.js
@@ -3,14 +3,19 @@ const technicians = require('../controllers/technicians.js');
 
 // Get all technicians
 router.get('/', technicians.findAll);
+
 //Get technicians by Attribute
 router.get('/attributes', technicians.findByAttr);
+
 //Get technicians by ID
 router.get('/:id', technicians.findOne);
+
 //Create a new technicians
 router.post('/', technicians.create);
+
 //Update technicians by ID
 router.put('/:id', technicians.update);
+
 //Delete technicians
 router.delete('/:id', technicians.delete);
 
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const port = 5000;
+const port = process.env.PORT || 5000;
 const router = require('./app/routes');
 const bodyParser = require('body-parser');
 const db = require("./app/models");
@@ -11,7 +11,6 @@ const db = require("./app/models");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.urlencoded({extended:false}));
-app.set('port', process.env.PORT || 3000);
 app.set('json spaces', 2);
 app.use(cors());
 app.use(router);
